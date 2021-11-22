530f1948883d6625a65cc1b81a8ed1225e3caff0
skyward27 <56093498+skyward27@users.noreply.github.com>
2021-11-22 04:44:13 -0500

    Update appParser.ts
    Changed code to right code for this file.

--- Backend/appParser.ts
+++ Backend/appParser.ts
@@ -1,57 +1,124 @@
-function  service(serviceName:string, input:string = ""){
-    var value:string = "";
-    ///build json service request and send to pi
-    ///process result
-    return value;
-}
+import * as fs from 'fs';
+import * as rd from 'readline';
 
-function control(serv1:string,serv2:string,input:string = ""){
-    var value:string = "";
-    var result:number = 0;
-    ///build first service json here and send to pi
-    ///store result in result
-    if(result > 0){
-        //build second service json and send to pi
-        //store result in result
-        value = result.toString();
-    }
-    else{
-        value = "";
-    }
-    return value;
+function appParser(inputFile:string){
+    var reader = rd.createInterface(fs.createReadStream(inputFile));
 
-}
-
-function drive(serv1:string, serv2:string,input:string = ""){
-    var value:string = "";
-    var result:number = 0;
-    //build first service json here and send to pi
-    //store result in result
-
-    //build second service json here with input result
-    //store result in value
-    return value;
-}
-
-function support(serv1:string,serv2:string,input:string = ""){
-    var value:string[] = ["",""];
-    ///build first service json here, store result in value[0]
-    //wait for return on first json, build second json, store result in value[1]
-    return value;
-}
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max)+1;
-  }
-
-function contest(serv1:string, serv2:string, input:string = ""){
-    var value:string = "";
-    var serviceSelector = getRandomInt(2);
-    if(serviceSelector = 1){
-        //build first service json, store result in value
-    }
-    else{
-        //build second service json, store result in value
-    }
-
+    reader.on("line",(l:string) => {
+        var tokens = l.split(" ");
+        switch(tokens[0]){
+            case("S"):
+            case("s"):
+            if (tokens.length > 2){
+                //call service maker with value of tokens[2]
+            }
+            else{
+                //call service maker with no value
+            }
+            case("R"):
+            case("r"):
+            //determine the relationship type by looking up token[1] in the relationship map
+            if(tokens.length > 2){
+                //call appropriate relationship function with value of token[2]
+            }
+            else{
+                //call appropriate realtionship function with no value
+            }
+            case("IF"):
+            case("If"):
+            case("if"):
+            if(tokens[1] == "S"||tokens[1] == 's'){
+                if(tokens[2] != 'S' && tokens[2] != 's' && tokens[2] != 'R' && tokens[2] != 'r'){
+                    //call service defined in token[1] with input defined in tokens[2]
+                    //if service returns value greater than 0, then execute next relationship
+                    if(service1.result > 0){
+                        if(tokens[3] == "S" || tokens[3] == 's'){
+                            if(tokens.length > 4){
+                                //call token[3] service with value token[4]
+                            }
+                            else{
+                                //call token[3]
+                            }
+                        }
+                        else{
+                            if(tokens.length > 4){
+                                //call token[3] relationship with value token[4]
+                            }
+                            else{
+                                //call token[3] relationship
+                            }
+                        }
+                    }
+                }
+                else{
+                    //call service defined in token[1] without input
+                    //if service returns value greater than 0, then execute next relationship
+                    if(service1.result > 0){
+                        if(tokens[2] == "S" || tokens[2] == 's'){
+                            if(tokens.length > 3){
+                                //call token[3] service with value token[4]
+                            }
+                            else{
+                                //call token[3]
+                            }
+                        }
+                        else{
+                            if(tokens.length > 3){
+                                //call token[3] relationship with value token[4]
+                            }
+                            else{
+                                //call token[3] relationship
+                            }
+                        }
+                    }  
+                }
+            }
+            else{
+                if(tokens[2] != 'S' && tokens[2] != 's' && tokens[2] != 'R' && tokens[2] != 'r'){
+                    //find what relationship token[1] entails and call appropriate realtionship driver
+                    //if relationship returns value greater than 0, then execute next relationship
+                    if(service1.result > 0){
+                        if(tokens[3] == "S" || tokens[3] == 's'){
+                            if(tokens.length > 4){
+                                //call token[3] service with value token[4]
+                            }
+                            else{
+                                //call token[3]
+                            }
+                        }
+                        else{
+                            if(tokens.length > 4){
+                                //call token[3] relationship with value token[4]
+                            }
+                            else{
+                                //call token[3] relationship
+                            }
+                        }
+                    }
+                }
+                else{
+                    //call service defined in token[1] without input
+                    //if service returns value greater than 0, then execute next relationship
+                    if(service1.result > 0){
+                        if(tokens[2] == "S" || tokens[2] == 's'){
+                            if(tokens.length > 3){
+                                //call token[3] service with value token[4]
+                            }
+                            else{
+                                //call token[3]
+                            }
+                        }
+                        else{
+                            if(tokens.length > 3){
+                                //call token[3] relationship with value token[4]
+                            }
+                            else{
+                                //call token[3] relationship
+                            }
+                        }
+                    }  
+                }
+            }
+        }
+    })
 }

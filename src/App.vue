b1947a34a5b1c04b0f807768709b6ed31d117f77
Harsh Soni <harshsoni@Harshs-MacBook-Air.local>
2021-11-24 21:38:16 -0500

    Add tab template

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,57 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <ejs-tab id='draggableTab' heightAdjustMode='Auto' :allowDragAndDrop="true" dragArea="#container">
+  <e-tabitems>
+      <e-tabitem :header="headerText0" :content="content0"></e-tabitem>
+      <e-tabitem :header="headerText1" :content="content1"></e-tabitem>
+      <e-tabitem :header="headerText2" :content="content2"></e-tabitem>
+  </e-tabitems>
+  </ejs-tab>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+
+import { TabComponent, TabItemsDirective, TabItemDirective } from "@syncfusion/ej2-vue-navigations";
+import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+/*import "../node_modules/@syncfusion/ej2-vue-buttons/styles/material.css";
+import "../node_modules/@syncfusion/ej2-vue-popups/styles/material.css";*/
+import "../node_modules/@syncfusion/ej2-vue-navigations/styles/material.css"
+
 import HelloWorld from "./components/HelloWorld.vue";
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+// @Options({
+//   components: {
+//     HelloWorld,
+//   },
+// })
+
+
+export default {
+     name: "App",
+     components: {
+       "ejs-tab": TabComponent,
+       "e-tabitems": TabItemsDirective,
+       "e-tabitem": TabItemDirective
+    },
+data() {
+return {
+
+    headerText0: { text: "Things" },
+    headerText1: { text: "Services" },
+    headerText2: { text: "Relationships" },
+    content0:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet nisl justo, et ullamcorper lacus pellentesque eu.",
+
+    content1:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet nisl justo, et ullamcorper lacus pellentesque eu.",
+
+    content2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet nisl justo, et ullamcorper lacus pellentesque eu."
+  }
+}
+}
 </script>
 
 <style>
@@ -24,4 +63,10 @@ export default class App extends Vue {}
   color: #2c3e50;
   margin-top: 60px;
 }
+  .e-tab .e-content .e-item {
+    padding: 10px;
+    text-align: justify;
+    font-size: 12px;
+}
 </style>
+

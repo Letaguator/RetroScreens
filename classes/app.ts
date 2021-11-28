08759a18189d5f6ce33045ae25b7d09a769b55ab
skyward27 <56093498+skyward27@users.noreply.github.com>
2021-11-28 13:24:27 -0500

    Create app.ts
    move app class to its own file

---
+++ classes/app.ts
@@ -0,0 +1,22 @@
+
+export class App{
+    name:string;
+    flow:string[];
+    picture:string;
+    constructor(nName:string, nFlow:string[]){
+        this.name = nName;
+        this.flow = nFlow;
+    }
+    getName(){
+        return this.name;
+    }
+    getFlow(){
+        return this.flow;
+    }
+    getPicture(){
+        return this.picture;
+    }
+    updatePicture(newPath:string){
+        this.picture = newPath; 
+    }
+}

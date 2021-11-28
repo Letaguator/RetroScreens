21677fb9ddb353e85fb637bf21fab7871c21dbd2
Harsh Soni <harshsoni@Harshs-MacBook-Air.local>
2021-11-28 18:51:38 -0500

    add relationships.vue- added form pop-up

--- src/components/Relationships.vue
+++ src/components/Relationships.vue
@@ -17,6 +17,42 @@
   </li>
   </ul>
 </div>
+
+<button class="open-button" v-on:click="openForm()">Add New</button>
+
+<div class="form-popup" id="myForm">
+  <form action="/action_page.php" class="form-container">
+    <h4>Create a Relationship</h4>
+
+    <label for="ServiceName">Service Name</label>
+    <input type="text" placeholder="Select here" name="ServiceName" required>
+
+    <label for="ServiceType">Service Type</label>
+    <select name="ServiceType" id="ServiceType">
+      <option value="Control">Control</option>
+      <option value="Drive">Drive</option>
+      <option value="Test">Test</option>
+      <option value="Interrupt">Interrupt</option>
+    </select>
+    <br><br>
+    <label for="ServiceType">Select Service 1</label>
+    <select name="ServiceType" id="ServiceType">
+      <option value="Control">Random</option>
+      <option value="Drive">Random</option>
+    </select>
+    <br><br>
+    <label for="ServiceType">Select Service 2</label>
+    <select name="ServiceType" id="ServiceType">
+      <option value="Control">Random</option>
+      <option value="Drive">Random</option>
+    </select>
+    <br><br>
+    <button type="submit" class="btn">Add</button>
+    <button type="button" class="btn cancel" v-on:click="closeForm()">Cancel</button>
+  </form>
+</div>
+
+
 </template>
 
 <script>
@@ -40,6 +76,12 @@ export default {
       } else {
         content.style.display = "block";
       }
+    },
+    openForm: function (){
+      document.getElementById("myForm").style.display = "block";
+    },
+    closeForm: function() {
+      document.getElementById("myForm").style.display = "none";
     }
   }
 };
@@ -68,5 +110,70 @@ export default {
 /* Add rounded corners to the top left and the top right corner of the image */
 img {
   border-radius: 5px 5px 0 0;
+}
+
+.open-button {
+  background-color: #555;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  opacity: 0.8;
+  bottom: 23px;
+  right: 28px;
+  width: 280px;
+}
+
+/* The popup form - hidden by default */
+.form-popup {
+  display: none;
+  bottom: 0;
+  right: 15px;
+  border: 3px solid #f1f1f1;
+  z-index: 9;
+}
+
+/* Add styles to the form container */
+.form-container {
+  max-width: 300px;
+  padding: 10px;
+  background-color: white;
+}
+
+/* Full-width input fields */
+.form-container input[type=text], .form-container input[type=password] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  border: none;
+  background: #f1f1f1;
+}
+
+/* When the inputs get focus, do something */
+.form-container input[type=text]:focus, .form-container input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Set a style for the submit/login button */
+.form-container .btn {
+  background-color: #04AA6D;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  margin-bottom:10px;
+  opacity: 0.8;
+}
+
+/* Add a red background color to the cancel button */
+.form-container .cancel {
+  background-color: red;
+}
+
+/* Add some hover effects to buttons */
+.form-container .btn:hover, .open-button:hover {
+  opacity: 1;
 }
 </style>

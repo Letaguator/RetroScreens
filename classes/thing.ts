afa9ea3c90a59db652881aa341a440952069f522
skyward27 <56093498+skyward27@users.noreply.github.com>
2021-11-28 13:25:08 -0500

    Create thing.ts
    move thing class to its own file

---
+++ classes/thing.ts
@@ -0,0 +1,27 @@
+export class Thing{
+    name:string;
+    ipAddress:string;
+    socket:string
+    picture:string;
+    constructor(nName:string,nAddress:string,nSocket){
+        this.name = nName;
+        this.ipAddress = nAddress;
+        this.socket = nSocket;
+        this.picture = '';
+    }
+    getName(){
+        return this.name;
+    }
+    getAddress(){
+        return this.ipAddress;
+    }
+    getSocket(){
+        return this.socket;
+    }
+    getPicture(){
+        return this.picture;
+    }
+    updatePicture(path:string){
+        this.picture = path;
+    }
+}

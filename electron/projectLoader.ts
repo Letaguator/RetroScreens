1994ce44f232e3570cbd3ac8a698db244fa9c92e
Mathias Brekkan <mathias.brekkan@outlook.com>
2021-12-01 22:05:42 -0500

    Add support for saving relationships to file

--- electron/projectLoader.ts
+++ electron/projectLoader.ts
@@ -4,9 +4,9 @@ import { app } from "electron";
 
 let workDir = app.getPath("userData") + path.sep + "WorkDir";
 let appDir = workDir + path.sep + "Apps";
+let relationshipsDir = workDir + path.sep + "Relationships";
 
 export function loadAppsFromWorkDirectory() {
-
     if (!fs.existsSync(workDir))
     {
         fs.mkdirSync(workDir, {recursive: true});
@@ -29,12 +29,42 @@ export function loadAppsFromWorkDirectory() {
 }
 
 export function saveAppToWorkDirectory(appData: any) {
-    fs.writeFileSync(appDir + path.sep + appData.name + ".viot", appData.src);
+    const savePath = appDir + path.sep + appData.name + ".viot";
+    fs.writeFileSync(savePath, appData.src);
 }
+
 export function deleteAppFromWorkDirectory(appName: string) {
     const fileToDelete = appDir + path.sep + appName + ".viot";
     if (fs.existsSync(fileToDelete))
     {
         fs.rmSync(fileToDelete);
     }
+}
+
+export function saveRelationship(relationshipData: any) {
+    const savePath = relationshipsDir + path.sep + relationshipData.name + ".rl";
+    fs.writeFileSync(savePath, relationshipData.jsonData);
+}
+
+export function loadRelationships() {
+    if (!fs.existsSync(workDir))
+    {
+        fs.mkdirSync(workDir, { recursive: true });
+    }
+
+    if (!fs.existsSync(relationshipsDir))
+    {
+        fs.mkdirSync(relationshipsDir, { recursive: true });
+    }
+
+    const relationshipFiles = fs.readdirSync(relationshipsDir);
+    const rawRelationships = [] as Array<Object>;
+    for(const relationshipFile of relationshipFiles)
+    {
+        const relName = relationshipFile.split(".")[0];
+        const jsonData = fs.readFileSync(relationshipsDir + path.sep + relationshipFile, "utf8");
+        rawRelationships.push({ name: relName, jsonData: jsonData })
+    }
+
+    return rawRelationships;
 }
